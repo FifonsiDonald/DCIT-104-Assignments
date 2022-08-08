9a743ee51116f7a848695042948e0b3cfc8e06c4
FifonsiDonald <102973424+FifonsiDonald@users.noreply.github.com>
2022-08-08 12:30:59 +0000

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,23 @@
+////assignment 10975107////
+
+package Assignment;
+
+public class assignment { 
+    public static void main(String[] args) {
+        int max=10000;
+        int average=0;
+        int sum=0 ;
+        int u=max/2;
+       { for(int i=2; i<=max; i+=2)
+       sum= sum+i;
+       
+       
+        
+        average= sum/u;
+        System.out.println("the average of the sum of integers from 1 to 10000 is");
+    System.out.println(average);
+       } 
+        
+        return ;
+    }
+}
